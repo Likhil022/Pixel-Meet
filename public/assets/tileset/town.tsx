ecadadffb8116d8d6629aa641bfb95d0fc815166
likhil022 <likhilkosuru123@gmail.com>
2025-07-11 09:14:56 +0530

    Deleted tileset.tsx file

--- public/assets/tileset/town.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="town" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="1680" columns="56">
- <image source="tiles.png" width="968" height="526"/>
-</tileset>
