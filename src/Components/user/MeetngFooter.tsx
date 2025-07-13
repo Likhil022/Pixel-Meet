ad706fdac59fa298c4bbecb6d7a70cbe7c4b4a3a
likhil022 <likhilkosuru123@gmail.com>
2025-07-13 16:52:10 +0530

    Meeting Room frame is Fixed

--- src/Components/user/MeetngFooter.tsx
+++ src/Components/user/MeetngFooter.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { useUser } from "@clerk/clerk-react";
 
 const MeetngFooter = () => {
-  return <div></div>;
+  const { user, isSignedIn } = useUser();
+
+  return (
+    <div>
+      <div>
+        <p>PM</p>
+        {isSignedIn && user && (
+          <div className="flex items-center gap-2">
+            <img
+              src={user.imageUrl}
+              alt="User Avatar"
+              className="w-8 h-8 rounded-full"
+            />
+            <span>{user.fullName}</span>
+          </div>
+        )}
+      </div>
+      <div></div>
+    </div>
+  );
 };
 
 export default MeetngFooter;
